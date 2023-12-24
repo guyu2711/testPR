fc7f73c22b98bb98ca733295b6b3feab86d454a4
guyu2711 <guyu2711@outlook.com>
2023-12-24 19:08:19 +0800

    add new struct

--- somefile1.py
+++ somefile1.py
@@ -1,2 +1,17 @@
 #somecode
 import numpy
+import pandas
+
+class MyStruct:
+    def __init__(self, field1, field2, field3):
+        self.field1 = field1
+        self.field2 = field2
+        self.field3 = field3
+
+# 使用这个结构体
+my_struct_instance = MyStruct(1, 'data', True)
+
+# 访问和修改属性
+print(my_struct_instance.field1)  # 输出 1
+my_struct_instance.field2 = 'new data'
+print(my_struct_instance.field2)  # 输出 'new data'
